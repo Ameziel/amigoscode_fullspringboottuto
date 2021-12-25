b5f7bd80d9b4fdce543ad525c44082a23ba77190
Cliel <samuel.sicard.it@gmail.com>
2021-12-25 14:20:30 +0100

    final

--- src/main/java/com/example/demo/students/StudentController.java
+++ src/main/java/com/example/demo/students/StudentController.java
@@ -35,6 +35,7 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
+
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long id) {
         studentService.deleteStudent(id);
